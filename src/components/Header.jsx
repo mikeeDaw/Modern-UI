56c1fa08ef85a4d2cabfaa46bc590f8891b5c123
mikeeDaw <mikeewazowski@gmail.com>
2024-10-16 17:39:23 +0800

    Study Completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -48,7 +48,7 @@ const Header = () => {
         <nav
           className={`${
             openNav ? "flex" : "hidden"
-          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-trasparent`}
+          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
